be500da10d4d98c0f8aafc30b50a62b533e04d57
Kate Basile <kathrynbasil5@gmail.com>
2024-04-04 16:05:25 -0400

    Initial Commit

---
+++ wesplit01/wesplit01App.swift
@@ -0,0 +1,17 @@
+//
+//  wesplit01App.swift
+//  wesplit01
+//
+//  Created by Katie on 4/4/24.
+//
+
+import SwiftUI
+
+@main
+struct wesplit01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
